f63e64a3b1682c5fcc2cfaf7088557422c8211d5
bukiyebarak <bukiyebarak810@gmail.com>
2023-08-01 16:17:42 +0300

    edit

--- functions/css.php
+++ functions/css.php
@@ -6,16 +6,10 @@ function tst_backend_css(){
 	echo '</style>';
 }
 add_action('admin_head','tst_backend_css');
-
 function tst_frontend_css(){
 	#tst-frontend-gdpr of ID number
 	wp_enqueue_style('tst-frontend-gdpr', plugin_dir_url(__DIR__).'html/css/gdpr-style.css');
 }
 add_action('wp_head','tst_frontend_css');
-
-
-
-
-
 
 ?>
